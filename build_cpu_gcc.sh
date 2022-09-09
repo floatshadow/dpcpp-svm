c70d28f99a7955eec97f97f35301b2b8b5870100
Zhu Siyuan <3200100491@zju.edu.cn>
2022-09-09 16:23:43 +0100

    Update build scripts

--- build_cpu_gcc.sh
+++ build_cpu_gcc.sh
@@ -2,6 +2,6 @@ rm -rf build
 
 CC=g++
 
-mkdir build && cd build && cmake -DCMAKE_CXX_COMPILER=$CC -DUSE_GPU=OFF -DUSE_CUDA=OFF -DUSE_PAPI=OFF -DCMAKE_CXX_FLAGS="-fopenmp" -DCMAKE_MODULE_PATH=. .. && make -j
+mkdir build && cd build && cmake -DCMAKE_CXX_COMPILER=$CC -DUSE_GPU=OFF -DUSE_CUDA=OFF -DUSE_PAPI=OFF -DCMAKE_CXX_FLAGS="-fopenmp -O3" -DCMAKE_MODULE_PATH=. .. && make -j
 
 
